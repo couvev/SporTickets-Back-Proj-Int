0ecf493ac01e22fb6c414b4f7dcfb18e7c2254a6
Joao Vitor Tomas Marra <joao.marra@sempreceub.com>
2025-02-03 20:56:46 -0300

    refactor: remove unecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { BlobModule } from './blob/blob.module';
-import { AppConfigModule } from './config/config.module';
 import { PrismaService } from './prisma/prisma.service';
-import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, BlobModule, AppConfigModule, ProductModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
