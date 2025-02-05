c254b382fe9631c99e93bc1f5612f79de0aa6a47
Guilherme Carramilo <guicarramilo@sempreceub.com>
2025-02-05 11:14:58 -0300

    feat: add configuration module and service for global settings management

---
+++ src/config/config.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get blobToken(): string {
+    return this.configService.get<string>('BLOB_READ_WRITE_TOKEN') || '';
+  }
+}
