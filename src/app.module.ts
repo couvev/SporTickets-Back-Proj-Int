6b8cc4b80368661b239c16286a21ae32cb1026d9
Guilherme Carramilo <guicarramilo@sempreceub.com>
2025-02-09 19:38:38 -0300

    feat: configure Swagger documentation for the API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BlobModule } from './blob/blob.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [AuthModule, BlobModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
