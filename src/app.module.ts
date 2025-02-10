a8ff9f568b7a5900bf36e8c3298b14db48c340cb
Ricardo Bittar <ricardosbittar@gmail.com>
2025-02-09 23:11:51 -0300

    feat: add EventModule to the application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BlobModule } from './blob/blob.module';
 import { PrismaService } from './prisma/prisma.service';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [AuthModule, BlobModule],
+  imports: [AuthModule, BlobModule, EventModule],
   controllers: [],
   providers: [PrismaService],
 })
