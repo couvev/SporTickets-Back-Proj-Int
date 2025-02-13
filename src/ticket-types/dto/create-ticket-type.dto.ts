5476af19f732ee994a904354bd5f57980206add2
Gabriel Pahl <gabriel.pahl@sempreceub.com>
2025-02-13 15:12:34 -0300

    feat: add ApiProperty decorators to DTOs for better Swagger documentation

--- src/ticket-types/dto/create-ticket-type.dto.ts
+++ src/ticket-types/dto/create-ticket-type.dto.ts
@@ -1,28 +1,35 @@
-import { IsNotEmpty, IsOptional, IsString, IsEnum } from 'class-validator';
-import { Restriction, UserType, Mode } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { Mode, Restriction, UserType } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateTicketTypeDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'event-id' })
   eventId: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'Ticket Type Name' })
   name: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Ticket Type Description' })
   description?: string;
 
   @IsOptional()
   @IsEnum(Restriction)
+  @ApiProperty({ example: 'AGE' })
   restriction?: Restriction;
 
   @IsOptional()
   @IsEnum(UserType)
+  @ApiProperty({ example: 'VIEWER' })
   userType?: UserType;
 
   @IsOptional()
   @IsEnum(Mode)
+  @ApiProperty({ example: 'SOLO' })
   mode?: Mode;
 }
