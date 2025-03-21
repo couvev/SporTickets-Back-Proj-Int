0f3f161bf02183ca95f8de70d98042c197f73afa
Ricardo Bittar <ricardosbittar@gmail.com>
2025-03-20 23:01:22 -0300

    feat: remove unused Coupon module imports and controllers from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { BlobModule } from './blob/blob.module';
 import { BracketModule } from './bracket/bracket.module';
 import { CategoryModule } from './category/category.module';
+import { CouponModule } from './coupon/coupon.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { EventModule } from './event/event.module';
 import { PersonalizedFieldsModule } from './personalized-fields/personalized-fields.module';
@@ -10,9 +11,6 @@ import { PrismaService } from './prisma/prisma.service';
 import { TicketLotModule } from './ticket-lot/ticket-lot.module';
 import { TicketTypeModule } from './ticket-types/ticket-types.module';
 import { UserModule } from './user/user.module';
-import { NestController } from './coupon/nest/nest.controller';
-import { CouponService } from './coupon/coupon.service';
-import { CouponModule } from './coupon/coupon.module';
 
 @Module({
   imports: [
@@ -29,7 +27,7 @@ import { CouponModule } from './coupon/coupon.module';
     TicketLotModule,
     CouponModule,
   ],
-  controllers: [NestController],
-  providers: [PrismaService, CouponService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
