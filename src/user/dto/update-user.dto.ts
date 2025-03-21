d22c3d1837fe8f28330d4d907f36cc75259280e2
Ricardo Bittar <ricardosbittar@gmail.com>
2025-03-21 14:44:32 -0300

    feat: enhance UpdateUserDto with validation rules for user fields

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,15 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Sex } from '@prisma/client';
-import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty({ example: 'Lucas Rosa', description: 'User full name.' })
   name?: string;
 
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
+  @MinLength(11)
   @ApiProperty({
     example: '0584303139',
     description: 'User document (e.g., CPF).',
@@ -18,20 +30,26 @@ export class UpdateUserDto {
 
   @IsOptional()
   @IsEmail()
+  @IsNotEmpty()
   @ApiProperty({ example: 'lucass@gmail.com', description: 'User email.' })
   email?: string;
 
   @IsOptional()
+  @IsNotEmpty()
+  @IsDateString()
   @ApiProperty({ example: '1990-01-01', description: 'User date of birth.' })
   bornAt?: Date;
 
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   @ApiProperty({ example: '12345-678', description: 'User postal code (CEP).' })
   cep?: string;
 
   @IsOptional()
   @IsEnum(Sex)
+  @IsNotEmpty()
   @ApiProperty({
     example: 'MALE',
     description: 'User gender (MALE ou FEMALE).',
@@ -40,6 +58,8 @@ export class UpdateUserDto {
 
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
+  @MinLength(11)
   @ApiProperty({ example: '61995585555', description: 'User phone number.' })
   phone?: string;
 }
