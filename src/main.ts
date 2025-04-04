68db9657984a5c8d32b61300d74aa271c7f859d5
Gabriel Pahl <gabriel.pahl@sempreceub.com>
2025-04-03 23:51:46 -0300

    feat: add additional origin to CORS configuration for frontend access

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://beta.sportickets.com.br'],
+    origin: [
+      'http://localhost:3000',
+      'https://beta.sportickets.com.br',
+      'https://spor-tickets-front.vercel.app',
+    ],
     credentials: true,
   });
 
