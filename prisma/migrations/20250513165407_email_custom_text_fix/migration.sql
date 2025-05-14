69d518d8a75fdf01539f1d372a7abdacca3577f5
Thiago Lucas Alves <thiagolcsalves@gmail.com>
2025-05-14 11:09:46 -0300

    feat: remove wrong migration

--- prisma/migrations/20250513165407_email_custom_text_fix/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `EmailCustomText` on the `Event` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Event" DROP COLUMN "EmailCustomText",
-ADD COLUMN     "emailCustomText" TEXT;
